c2e875448a02fd5dbe54ab4af5ba97e3b7318dfd
Ernad Husremovic <hernad@bring.out.ba>
2012-03-08 12:57:18 +0100

    pametan download md5_sum funkcija

--- scripts/download_box_from_gcode.sh
+++ scripts/download_box_from_gcode.sh
@@ -4,34 +4,47 @@ FILE_NAME=precise-desktop-i386.box
 
 URL=http://knowhow-erp.googlecode.com/files
 
+md5_sum() 
+{
+echo "provjeravam $FILE_NAME md5 sumu" 
+
+RET=`md5sum $FILE_NAME`
+
+if [[ "$RET" ==  "97202c5fdbfcb1ad543c8decbec36994  $FILE_NAME" ]]
+then
+ echo "md5 sum ok, brisem dijelove a-h" 
+ rm $FILE_NAME.part_a[a-h] 2> /dev/null
+ MD5_OK=1
+else
+ echo "md5 sum ne valja, brisem box fajl !" 
+ rm $FILE_NAME 2> /dev/null
+ MD5_OK=0
+fi
+
+}
+
 download_merge_parts()
 {
 
 for item in aa ab ac ad ae af ag ah
 do
     echo wget $URL/$FILE_NAME.part_$item
-    wget -c -nc $URL/$FILE_NAME.part_$item
+    wget --continue  $URL/$FILE_NAME.part_$item
 done
 
 cat $FILE_NAME.part_a[a-h] > $FILE_NAME
 
-
-RET=`md5sum $FILE_NAME`
-
-
-if [[ "$RET" ==  "97202c5fdbfcb1ad543c8decbec36994  $FILE_NAME" ]]
-then
- echo "md5 sum ok, brisem dijelove a-h"
- rm $FILE_NAME.part_a[a-h]
-fi
-
-
-
+md5_sum
 }
 
-if [[ ! -f $FILE_NAME ]]
-then
-   download_merge_parts
-  
-fi 
+MD5_OK=0
 
+if [[ -f $FILE_NAME ]]
+then 
+ md5_sum
+fi
+
+if [[ Md5_OK -eq 0 ]]
+then
+   download_merge_parts  
+fi 
