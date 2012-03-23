00d111534a018f20e8c94c727d89e51c31f6a6c6
Ernad Husremovic <hernad@bring.out.ba>
2012-03-23 16:47:53 +0100

    lxde

--- scripts/download_box_from_gcode.sh
+++ scripts/download_box_from_gcode.sh
@@ -3,16 +3,29 @@
 BOX_NAME=$1
 FILE_NAME=$BOX_NAME.box
 
+
+DARWIN=`uname -a | grep -c Darwin`
+
+if [[ "$DARWIN" == "1" ]]
+then
+    SHA_CMD=shasum
+else
+    SHA_CMD=sha1sum
+fi
+
 case "$BOX_NAME" in
    
 precise-desktop-i386)
    FILE_SIZE=925013504
-   PART_NAMES=aa ab ac ad ae af ag ah
+   PART_NAMES="aa ab ac ad ae af ag ah"
+   PARTS_RANGE=a-h
    ;;
 
 precise-desktop-lxde)
    FILE_SIZE=425013504
-   PART_NAMES=aa ab ac ad
+   PART_NAMES="aa ab ac ad"
+   PARTS_RANGE=a-d
+   ;;
 
 *)  exit 1
     ;;
@@ -26,7 +39,7 @@ sha1_sum()
 {
 echo "provjeravam $FILE_NAME sha1 sume"
 
-RET=`sha1sum -c scripts/${BOX_NAME}_parts.sha1`
+RET=`$SHA_CMD -c scripts/${BOX_NAME}_parts.sha1`
 
 if [[ $? -eq 0 ]]
 then
@@ -48,13 +61,17 @@ for item in $PART_NAMES
 do
     echo wget $URL/$FILE_NAME.part_$item
     wget --continue  $URL/$FILE_NAME.part_$item
+    if [[ ! -f $FILE_NAME.part_$item ]] ; then
+       echo pokusavam wget  $URL/${FILE_NAME}_part_${item}
+       wget --continue  $URL/${FILE_NAME}_part_${item} -O $FILE_NAME.part_$item
+    fi
 done
 
-cat $FILE_NAME.part_a[a-h] > $FILE_NAME
+cat $FILE_NAME.part_a[$PARTS_RANGE] > $FILE_NAME
 
 sha1_sum
 
-if [[  $SH1SUM_OK == "1" ]]
+if [[  "$SH1SUM_OK" == "1" ]]
 then
    echo brisem $FILE_NAME download-ovane dijelove ... vise mi ne trebaju.
    rm $FILE_NAME.part_a[a-h]
@@ -72,6 +89,8 @@ then
   echo brisem ga ...
   rm $FILE_NAME
 fi
+
+echo $FILE_NAME
 
 if [[ ! -f $FILE_NAME ]]
 then
