48ac1e7d43ade5224a2205a95aa4fccaf489f486
Ernad Husremovic <hernad@bring.out.ba>
2012-03-25 13:18:51 +0200

    build lxde box ponovo

--- scripts/build_box_desktop-precise-i386.sh
+++ scripts/build_box_desktop-precise-i386.sh
@@ -1,1 +1,1 @@
-vagrant package --vagrantfile Vagrantfile.pkg --output precise-desktop-i386.box
+vagrant package --vagrantfile Vagrantfile.pkg --output precise-desktop-i386.box f18_dev_1
--- scripts/build_box_desktop-precise-lxde.sh
+++ scripts/build_box_desktop-precise-lxde.sh
@@ -1,1 +1,1 @@
-vagrant package --vagrantfile Vagrantfile.pkg --output precise-desktop-lxde.box
+vagrant package --vagrantfile Vagrantfile.pkg --output precise-desktop-lxde.box f18_dev_2
