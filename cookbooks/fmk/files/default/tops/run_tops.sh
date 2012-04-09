de0097fe0c86a40a2d897d0aaa72211725330d88
Ernad Husremovic <hernad@bring.out.ba>
2012-04-09 23:37:36 +0200

    kill tops, gateway

--- cookbooks/fmk/files/default/tops/run_tops.sh
+++ cookbooks/fmk/files/default/tops/run_tops.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+RUN_TOPS=`ps ax | grep -v grep | grep -c dosemu.*tops.*ee`
+
+if [[ ! $RUN_TOPS -eq 0 ]]
+then
+    sudo killall dosemu
+fi
+
 sudo dosemu -E c:\\tops\\ee_dosem.bat &
